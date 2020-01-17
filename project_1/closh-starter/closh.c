c7b250cbfb2534ce3011f5f1e2a800616bcc5666
ricksonre <ricksonre@gmail.com>
2020-01-17 15:41:40 -0800

    Update closh.c

--- project_1/closh-starter/closh.c
+++ project_1/closh-starter/closh.c
@@ -11,45 +11,52 @@
 #define false 0
 
 // tokenize the command string into arguments - do not modify
-void readCmdTokens(char* cmd, char** cmdTokens) {
+void readCmdTokens(char* cmd, char** cmdTokens) 
+{
     cmd[strlen(cmd) - 1] = '\0'; // drop trailing newline
     int i = 0;
     cmdTokens[i] = strtok(cmd, " "); // tokenize on spaces
-    while (cmdTokens[i++] && i < sizeof(cmdTokens)) {
+    while (cmdTokens[i++] && i < sizeof(cmdTokens)) 
+	{
         cmdTokens[i] = strtok(NULL, " ");
     }
 }
 
 // read one character of input, then discard up to the newline - do not modify
-char readChar() {
+char readChar() 
+{
     char c = getchar();
     while (getchar() != '\n');
     return c;
 }
 
 // main method - program entry point
-int main() {
+int main() 
+{
     char cmd[81]; // array of chars (a string)
     char* cmdTokens[20]; // array of strings
     int count; // number of times to execute command
     int parallel; // whether to run in parallel or sequentially
     int timeout; // max seconds to run set of commands (parallel) or each command (sequentially)
     
-    while (TRUE) { // main shell input loop
+    while (true) 
+	{ // main shell input loop
         
         // begin parsing code - do not modify
         printf("closh> ");
         fgets(cmd, sizeof(cmd), stdin);
         if (cmd[0] == '\n') continue;
         readCmdTokens(cmd, cmdTokens);
-        do {
+        do 
+		{
             printf("  count> ");
             count = readChar() - '0';
         } while (count <= 0 || count > 9);
         
         printf("  [p]arallel or [s]equential> ");
         parallel = (readChar() == 'p') ? TRUE : FALSE;
-        do {
+        do 
+		{
             printf("  timeout> ");
             timeout = readChar() - '0';
         } while (timeout < 0 || timeout > 9);
