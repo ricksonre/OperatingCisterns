87990690a35361423fcaa6877987a69760292981
Kaze <renyuan7777@gmail.com>
2020-02-29 16:44:33 -0800

    Fixed

--- project_2/Mini Project 2/src/main/Request.java
+++ project_2/Mini Project 2/src/main/Request.java
@@ -2,7 +2,7 @@ package main;
 
 import java.util.Random;
 
-public class Request {
+public class Request implements Runnable {
 	static int requestID;
 	int ID;
 	int length;
@@ -11,6 +11,20 @@ public class Request {
 		Random r = new Random();
 		this.ID = Request.requestID++;
 		this.length = r.nextInt(maxDur);
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		try {
+			System.out.println("Task "+ID+" running");
+			Thread.sleep(length);
+			System.out.println("Task "+ID+" complete");
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
