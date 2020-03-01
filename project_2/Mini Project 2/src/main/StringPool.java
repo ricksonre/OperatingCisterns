b369750db7daa08336b6d4a2dc1dce4f902485e1
Kaze <renyuan7777@gmail.com>
2020-02-29 17:15:51 -0800

    Sp

--- project_2/Mini Project 2/src/main/StringPool.java
+++ project_2/Mini Project 2/src/main/StringPool.java
@@ -1,5 +1,35 @@
 package main;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.locks.Lock;
+
 public class StringPool {
+	Lock[] lk;
+	Thread[] tp;
+	Queue<Thread> q = new LinkedList<>();
+	
+	
+	public StringPool(int size) {
+		tp = new Thread[size];
+		lk = new Lock[size];
+		
+	}
+	
+	
+	public synchronized void runTask(Runnable r) {
+		
+		
+		
+	}
+	
+	private synchronized int findOpenLock() {
+		for(int i = 0; i< lk.length; i++) {
+			if(lk[i].tryLock()) {
+				return i;
+			}
+		}
+		return -1;
+	}
 
 }
