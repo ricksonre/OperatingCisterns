89643c8a5ae9121a573996d6d1c9efd1f80946d1
Kaze <renyuan7777@gmail.com>
2020-02-29 18:39:13 -0800

    Thread

--- project_2/Mini Project 2/src/main/MainThread.java
+++ project_2/Mini Project 2/src/main/MainThread.java
@@ -6,17 +6,17 @@ import java.util.concurrent.Executors;
 
 public class MainThread implements Runnable{
 	Random r = new Random();
-	ExecutorService ex = Executors.newFixedThreadPool(5);
+	ThreadPool ex = new ThreadPool(6);
 	
 
 	@Override
-	public synchronized void run() {
+	public  void run() {
 		// TODO Auto-generated method stub
 		int j = 10; 
 		while (j-->0) {
 			
-			ex.execute(new Request(2000));
-			
+			ex.execute(new Request(10000));
+			ex.execute(new Request(10000));
 			try {
 				System.out.println("Main sleeping after request "+(10-j));
 				Thread.sleep(r.nextInt(1000));
